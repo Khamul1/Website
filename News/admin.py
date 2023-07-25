62240acf72ad43f729fa54ad17c5a0879680acf8
Khamul1 <rositlsav.kostencko@gmail.com>
2023-07-25 23:11:11 +0300

    все работает

--- News/admin.py
+++ News/admin.py
@@ -3,11 +3,12 @@ from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
+
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id','category','title','content','created_at','get_photo','is_published')
-    list_display_links = ('id','title')
+    list_display = ('id', 'category', 'title', 'content', 'created_at', 'get_photo', 'is_published')
+    list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
-    list_filter = ('is_published','id')
+    list_filter = ('is_published', 'id')
     list_editable = ['is_published', 'category']
     fields = ('title', 'content', 'photo', 'get_photo', 'is_published', 'created_at', 'updated_at')
     readonly_fields = ('get_photo', 'created_at', 'updated_at')
@@ -20,13 +21,15 @@ class NewsAdmin(admin.ModelAdmin):
 
     get_photo_description = 'Миниатюра'
 
-
+    def updated_at(self, obj):
+        return obj.updated_at
+    updated_at.short_description = 'Updated at'
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id','title')
-    list_display_links = ('id','title')
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
 
 
-admin.site.register(News,NewsAdmin)
+admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
 
 admin.site.site_title = 'Страница администратора'
