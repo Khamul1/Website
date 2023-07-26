3d5b84ffe9f21e0b4e7866df3ded04f6700a4235
Khamul1 <rositlsav.kostencko@gmail.com>
2023-07-26 22:16:22 +0300

    настроили static.css и admin.css

--- Django/settings.py
+++ Django/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILER_DIRS =[
+STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'Django/static')
 ]
 # Default primary key field type
